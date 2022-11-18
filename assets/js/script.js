2fc23f1fdfd0af6363ce60cdf1a616d5a70ac37d
EeeBeeDee <eoghanbehandoyle@gmail.com>
2022-11-18 17:24:29 +0000

    Basic html template and js structure

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const video_container = document.getElementById('video-container')
+const video = document.getElementById('video')
+const answers_container = document.getElementById('video-container')
+
+let songArr = [
+    {
+        song: "Hysteria",
+        src: "https://www.youtube.com/embed/3dm_5qWWDV8",
+        difficulty: "easy",
+        numberOfAnswers: 4,
+        correctAnswer: "4/4",
+        otherAnswers: ["3/4", "6/8", "4/3"],
+    },
+]
+
+function setSong(diff) {
+    
+}
